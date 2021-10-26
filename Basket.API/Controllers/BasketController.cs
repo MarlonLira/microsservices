131d6fa6ae4dc23f1ee01a55765e5ef3080f2270
Marlon Lira <marlonlira3@gmail.com>
2021-10-26 20:31:42 -0300

    Configuração do redis e criação das classes relacionadas a Basket

---
+++ Basket.API/Controllers/BasketController.cs
@@ -0,0 +1,44 @@
+﻿using Basket.API.Entities;
+using Basket.API.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace Basket.API.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class BasketController : ControllerBase
+    {
+        private readonly IBasketRepository _repository;
+
+        public BasketController(IBasketRepository repository)
+        {
+            _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+        }
+
+        [HttpGet]
+        [Route("{userName}")]
+        public async Task<ActionResult<ShoppingCart>> GetByUserName(string userName)
+        {
+            var shoppingCart = await _repository.GetByUserName(userName);
+
+            return Ok(shoppingCart ?? new ShoppingCart(userName));
+        }
+
+        [HttpPut]
+        [Route("")]
+        public async Task<ActionResult<ShoppingCart>> Update([FromBody] ShoppingCart shoppingCart)
+        {
+            return Ok(await _repository.Update(shoppingCart));
+        }
+
+        [HttpDelete]
+        [Route("{userName}")]
+        public async Task<ActionResult> Delete(string userName)
+        {
+            await _repository.DeleteByUserName(userName);
+            return Ok();
+        }
+    }
+}
---
+++ Basket.API/Repositories/BasketRepository.cs
@@ -0,0 +1,37 @@
+﻿using Basket.API.Entities;
+using Microsoft.Extensions.Caching.Distributed;
+using System;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Basket.API.Repositories
+{
+    public class BasketRepository : IBasketRepository
+    {
+        private readonly IDistributedCache _redis;
+
+        public BasketRepository(IDistributedCache redis)
+        {
+            _redis = redis ?? throw new ArgumentNullException(nameof(redis));
+        }
+
+        public async Task DeleteByUserName(string userName)
+        {
+            await _redis.RemoveAsync(userName);
+        }
+
+        public async Task<ShoppingCart> GetByUserName(string userName)
+        {
+            var shoppingCart = await _redis.GetStringAsync(userName);
+            if (string.IsNullOrEmpty(shoppingCart)) { return null; }
+
+            return JsonSerializer.Deserialize<ShoppingCart>(shoppingCart);
+        }
+
+        public async Task<ShoppingCart> Update(ShoppingCart shoppingCart)
+        {
+            await _redis.SetStringAsync(shoppingCart.UserName, JsonSerializer.Serialize(shoppingCart));
+            return await GetByUserName(shoppingCart.UserName);
+        }
+    }
+}
